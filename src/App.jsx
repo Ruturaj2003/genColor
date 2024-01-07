6220783d2b030d08944cbe23be3f761dfa699b0d
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-01-07 20:31:23 +0530

    Add Toast

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import Form from './Form';
 import ColorList from './ColorList';
 import Values from 'values.js';
 import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
 const App = () => {
   const [colors, setColors] = useState(new Values('#f4c3d2').all(10));
   console.log(colors);
+  toast.error('gg');
+  toast.success('Well Played');
   return (
     <main>
+      <ToastContainer position="top-right"></ToastContainer>
       <Form></Form>
       <ColorList colors={colors}></ColorList>
     </main>
